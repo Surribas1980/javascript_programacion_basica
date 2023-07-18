de216e8851afc71dcd3b59935a764d841309ad8c
Surribas1980 <issupl@gmail.com>
2023-07-18 15:30:37 +0000

    clase Programación básica

---
+++ Exercicios/clase_18_07_2023.js
@@ -0,0 +1,29 @@
+/*let cats = ['Bill','Jeff','Pete','Biggles','Jasmin']
+let myInfo = 'My cats are called '
+//console.log(cats.length)
+
+for(let contador = 0;contador < cats.length;contador ++){
+  myInfo += cats[contador] + ',';
+  console.log('O contador é: ',contador);
+  console.log(myInfo)
+}*/
+
+let casa = {
+  xanelas:10,
+  portas:5,
+  camas:10,
+  cociña:{
+    conxelador: 1,
+    armarios:10,
+    sillas:20
+  }
+}
+
+console.log(casa.xanelas,casa.portas,casa.camas);
+console.log(casa.cociña.sillas)
+
+
+/*for(let propiedade in casa){
+  console.log(`A propiedade é: ${propiedade}, e o valor ${casa[propiedade]}`)
+  
+}*/
