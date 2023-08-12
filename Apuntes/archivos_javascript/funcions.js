5f7d593ecdd95689f736e5c19631c9416694d426
Surribas1980 <issupl@gmail.com>
2023-08-12 11:12:58 +0000

    O tema de async e rl :) solutioned

--- Apuntes/archivos_javascript/funcions.js
+++ Apuntes/archivos_javascript/funcions.js
@@ -1,5 +1,47 @@
+import {rl} from './funcions.de.cabecera.js';
+
 function magicNumber(min, max){
 		return Math.floor(Math.random() * (max - min)) + min
 	}
 
-export {magicNumber}
+async function func(){
+  const pasarANum = await rl.question("Introduce o número de unidades que queres: ");
+
+  return pasarANum;
+}
+let miCallback = (result)=>{
+  if(parseInt(result) > 0){
+    func().then(miCallback) 
+  }else{
+    rl.close()
+  }
+    
+}
+
+export {magicNumber,func,miCallback}
+
+
+/*
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'O> ',
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  switch (line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`);
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+*/
---
+++ Apuntes/archivos_javascript/varios2.js
@@ -0,0 +1,4 @@
+
+import {func,miCallback} from './funcions.js';
+
+func().then(miCallback)
