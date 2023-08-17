ac3d9c50c7b1ccab3235c21b32c6b8629bdb59d3
Surribas1980 <issupl@gmail.com>
2023-08-17 08:33:48 +0000

    xoves 17 primeira hora

--- index.js
+++ index.js
@@ -23,10 +23,38 @@ export {
 }
 */
 
-let valor = 5;
+/*let datos = [1,2,3];
+let outrosDatos = [4,5,6];
+let arrayDeArrays = [datos,outrosDatos];
+console.log(`Este é o array de arrays ${arrayDeArrays}`)
+console.log(arrayDeArrays,' .... este é outro elemento individual: ',arrayDeArrays[0]);
+console.log(arrayDeArrays[0][1])
+console.log('lonxitude de arrayDeArrays[0] : ', arrayDeArrays[0].length)*/
+//console.log(datos[-2],datos.at(1),datos.at(-2))
+let creoArray = [];
+console.log('creoArray ',creoArray)
+console.log('engado elemento: ', creoArray.push(6))
+console.log(creoArray)
+console.log('engado elemento: ', creoArray.push(8))
+console.log(creoArray)
+console.log('engado elemento: ', creoArray.push(9))
+console.log(creoArray)
+console.log('saco elemento dende ...',creoArray.shift())
+console.log(creoArray)
+console.log('saco elemento dende ...',creoArray.shift())
+console.log(creoArray)
 
+console.log('agrego ...',creoArray.unshift("juan"))
+console.log(creoArray)
+console.log('agrego ...',creoArray.unshift("pepe",89))
+console.log(creoArray)
 
-valor > 5 ? console.log(`o valor é maior de 5`) : console.log('non é maior');
+/////////////////
+/*console.log('saco o elemento: ',creoArray.pop())
+console.log(creoArray)
+console.log('saco o elemento: ',creoArray.pop())
+console.log(creoArray)*/
+
 
 
 
